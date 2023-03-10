f11800ac32c20885c488d705119acc468e5d25e3
Harrisonvo <harrisonvo1@gmail.com>
2023-03-10 13:06:33 -0600

    Added userlogin page route

--- routes/userLogin.js
+++ routes/userLogin.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const path = require("path");
 const router = express.Router();
+const mongoose = require("mongoose");
 
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/pages/userLogin.html"));
