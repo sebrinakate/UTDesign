1fdffa210c2e302243a5558ca43d3109585fd5c4
Harrisonvo <harrisonvo1@gmail.com>
2023-03-10 13:01:46 -0600

    Fixed issue with html pages not being able to get stuff in frontend folder

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const path = require("path");
 const port = 3000;
 
+// Setting up static files
+// Makes sure that the frontend folder is accessible
+app.use(express.static(path.join(__dirname, "frontend")));
+
 // Importing routes
 const homeRoute = require("./routes/home.js");
 
