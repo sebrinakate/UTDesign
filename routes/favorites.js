953addb419ca31bc0f5bb8056a9c8651bc62a733
Vicky Nguyen <vickynguyen143@gmail.com>
2023-03-21 15:52:07 -0500

    added more code

--- routes/favorites.js
+++ routes/favorites.js
@@ -4,14 +4,21 @@ const path = require("path");
 // Creating express router  
 const router = express.Router();
 
-const student = await db.Student.findOne(ObjectID(studentID));
-const tutor = await db.Tutor.find(query).toArray();
-
 //for(let numFav of tutor){
     if(student.favorites[numFav.tutorID])
         // display list of favorited tutors
 //}
 
+// get favorites
+router.get("/favorites", async (req, res) => {
+    const studentID = req.session.studentID;
+    const query = { tutorID: studentID };
+
+    const student = await db.Student.findOne(ObjectID(studentID));
+    const tutor = await db.Tutor.find(query).toArray();
+
+    res.render("favorites", { student, tutor });
+});
 
 // Exporting router
 module.exports = router;
