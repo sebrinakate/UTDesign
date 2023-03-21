fd2647b82f1f2f6303111e580e3d8ce81b4c5f2d
Vicky <vickynguyen143@gmail.com>
2023-03-20 21:17:38 -0500

    updating favorites code

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,3 +1,9 @@
+const express = require("express");
+const path = require("path");
+
+// Creating express router  
+const router = express.Router();
+
 const student = await db.Student.findOne(ObjectID(studentID));
 const tutor = await db.Tutor.find(query).toArray();
 
@@ -5,3 +11,7 @@ for(let numFav of tutor){
     if(student.favorites[numFav.tutorID])
         // display list of favorited tutors
 }
+
+
+// Exporting router
+module.exports = router;
