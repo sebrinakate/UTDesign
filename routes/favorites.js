3525fd33675117d38c948667e89367a42f00a14b
Vicky Nguyen <vickynguyen143@gmail.com>
2023-03-28 15:07:11 -0500

    post

--- routes/favorites.js
+++ routes/favorites.js
@@ -20,5 +20,49 @@ router.get("/favorites", async (req, res) => {
     //}
 });
 
+// post favorites
+router.post("/favorites", async (req, res) => {
+    const studentID = req.session.studentID;
+    const tutorID = req.body.tutorID;
+
+    const student = await db.Student.findOne(ObjectID(studentID));
+    const tutor = await db.Tutor.findOne(ObjectID(tutorID));
+
+    if (student.favorites[tutorID]) {
+        delete student.favorites[tutorID];
+    } else {
+        student.favorites[tutorID] = tutor;
+    }
+
+    await db.Student.updateOne(
+        { _id: ObjectID(studentID) },
+        { $set: { favorites: student.favorites } }
+    );
+
+    res.redirect("/favorites");
+});
+
+// delete favorites
+router.delete("/favorites", async (req, res) => {
+    const studentID = req.session.studentID;
+    const tutorID = req.body.tutorID;
+
+    const student = await db.Student.findOne(ObjectID(studentID));
+    const tutor = await db.Tutor.findOne(ObjectID(tutorID));
+
+    if (student.favorites[tutorID]) {
+        delete student.favorites[tutorID];
+    } else {
+        student.favorites[tutorID] = tutor;
+    }
+
+    await db.Student.updateOne(
+        { _id: ObjectID(studentID) },
+        { $set: { favorites: student.favorites } }
+    );
+
+    res.redirect("/favorites");
+});
+
 // Exporting router
 module.exports = router;
