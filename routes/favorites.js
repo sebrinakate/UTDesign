f112c9db526cb1b8ad3127bef77815eee8fe262d
Vicky Nguyen <vickynguyen143@gmail.com>
2023-04-19 23:09:25 -0500

    update put

--- routes/favorites.js
+++ routes/favorites.js
@@ -15,6 +15,7 @@ router.get("/", async (req, res) => {
     // find student's favorite tutors
     try{
         //const favTutors = await studentModel.findById(studentID).select("favoriteTutors");
+        
         // this returns student id and favoriteTutors
         const favTutors = await studentModel.find({loggedIn: true}).select("favoriteTutors");
         res.send(favTutors);
@@ -25,28 +26,58 @@ router.get("/", async (req, res) => {
 });
 
 // ---------- put favorites ----------
-router.put("/", async (req, res) => {
-    const studentID = req.session.studentID;
-    const tutorID = req.body.tutorID;
+router.get("/peaches", async (req, res) => {
+    //const studentID = req.session.studentID;
+    //const tutorID = req.body.tutorID;
+    //const student = await studentModel.find({loggedIn: true});
+    //const tutor = await tutorModel.findbyId(tutorID);
 
-    const student = await studentModel.findOne(ObjectID(studentID));
-    const tutor = await tutorModel.findOne(ObjectID(tutorID));
+    // testing
+    const studentID = "64079e1948dede36ae877bfe";
+    const tutorID = "6435ba187c0414d71edd8e62";
+    const student = await studentModel.findById(studentID).select("favoriteTutors");
+    const tutor = await tutorModel.findById(tutorID)
 
-    // if tutor is already in favorites, remove it
-    if (student.favorites[tutorID]) {
-        delete student.favorites[tutorID];
-    } else {
+    try{
+        
+        // if tutor is already in favorites, remove it
+        if (student.favoriteTutors[tutorID]) {
+            
+            //delete student.favoriteTutors[tutorID];
+            //student.favoriteTutors.pull(tutor);
+
+            const favTutors = await studentModel.findOneAndUpdate(
+                // find student
+                { id: studentID },
+                // remove tutor to student favorites
+                { $pull: { favoriteTutors: tutor } },
+            );
+            res.send(favTutors)
+            
         // else add tutor to favorites
-        student.favorites[tutorID] = tutor;
+        } else {
+            
+            //student.favoriteTutors[tutorID] = tutor;
+            //student.favoriteTutors.push(tutor);
+
+            // update student favorites
+            const favTutors = await studentModel.findOneAndUpdate(
+                // find student
+                { id: studentID },
+                // add tutor to student favorites
+                { $addToSet: { favoriteTutors: tutor } },
+            );
+            res.send(favTutors)
+        }
+        
+        // save student
+        //student.save();
+    }
+    catch(err){
+        res.status(500).json({message: err.message});
     }
 
-    // update student favorites
-    await studentModel.updateOne(
-        // find student
-        { _id: ObjectID(studentID) },
-        // set favorites to student favorites
-        { $set: { favorites: student.favorites } }
-    );
+    
 
     // redirect to favorites
     res.redirect("/");
@@ -57,19 +88,27 @@ router.delete("/", async (req, res) => {
     const studentID = req.session.studentID;
     const tutorID = req.body.tutorID;
 
-    // find student
-    const student = await studentModel.findOne(ObjectID(studentID));
+    try{
 
-    // delete tutor from favorites
-    delete student.favorites[tutorID];
-
-    // update student favorites
-    await studentModel.updateOne(
         // find student
-        { _id: ObjectID(studentID) },
-        // set favorites to student favorites
-        { $set: { favorites: student.favorites } }
-    );
+        const student = await studentModel.findById(studentID);
+
+        // delete tutor from favorites
+        delete student.favoriteTutors[tutorID];
+
+        // update student favorites
+        await studentModel.findOneAndUpdate(
+            // find student
+            { id: ObjectID(studentID) },
+            // set favorites to student favorites
+            { $set: { favoriteTutors: tutor } }
+        );
+
+    }
+    catch(err){
+        res.status(500).json({message: err.message})
+    }
+
 
     // redirect to favorites
     res.redirect("/");
