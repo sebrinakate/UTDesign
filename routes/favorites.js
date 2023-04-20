9be56a919864cdf8c12633f05a94af7f446af3fb
Vicky Nguyen <vickynguyen143@gmail.com>
2023-04-20 02:07:47 -0500

    update

--- routes/favorites.js
+++ routes/favorites.js
@@ -34,55 +34,71 @@ router.get("/peaches", async (req, res) => {
 
     // testing
     const studentID = "64079e1948dede36ae877bfe";
-    const tutorID = "6435ba187c0414d71edd8e62";
-    const student = await studentModel.findById(studentID).select("favoriteTutors");
-    const tutor = await tutorModel.findById(tutorID)
+    const tutorID = "643efe14eea3bf3880f15a21";
+    const student = await studentModel.findById(studentID);
+    const tutor = await tutorModel.findById(tutorID).select("name");
+
+    const tutor_name = tutor.name.firstName + " " + tutor.name.lastName;
+    //const tutor_name = JSON.stringify(tutor.name);
+    var objTutor = {tutorID: tutorID, tutorName: tutor_name};
+
+    //const tst = student.favoriteTutors.includes(objTutor);
+    //res.send(tst)
+    //res.send(objTutor)
 
     try{
-        
+        /*
         // if tutor is already in favorites, remove it
-        if (student.favoriteTutors[tutorID]) {
+        if (student.favoriteTutors.includes(tutorID)) {
             
             //delete student.favoriteTutors[tutorID];
             //student.favoriteTutors.pull(tutor);
 
             const favTutors = await studentModel.findOneAndUpdate(
-                // find student
-                { id: studentID },
+                { _id: studentID },
                 // remove tutor to student favorites
-                { $pull: { favoriteTutors: tutor } },
+                { $pull: { favoriteTutors: objTutor } },
             );
-            res.send(favTutors)
             
+            res.send(favTutors)
+        }   
         // else add tutor to favorites
-        } else {
+        else {
             
             //student.favoriteTutors[tutorID] = tutor;
             //student.favoriteTutors.push(tutor);
 
             // update student favorites
             const favTutors = await studentModel.findOneAndUpdate(
-                // find student
-                { id: studentID },
+                { _id: studentID },
                 // add tutor to student favorites
-                { $addToSet: { favoriteTutors: tutor } },
+                { $addToSet: { favoriteTutors: objTutor} },
             );
+            
             res.send(favTutors)
         }
+        */
+
+        // update student favorites
+        const favTutors = await studentModel.findOneAndUpdate(
+            { _id: studentID },
+            // add tutor to student favorites
+            { $addToSet: { favoriteTutors: objTutor} },
+        );
         
-        // save student
-        //student.save();
+        res.send(favTutors)
+        
+
     }
     catch(err){
         res.status(500).json({message: err.message});
     }
 
-    
-
     // redirect to favorites
     res.redirect("/");
 });
 
+/*
 // ---------- delete favorites ----------
 router.delete("/", async (req, res) => {
     const studentID = req.session.studentID;
@@ -99,7 +115,7 @@ router.delete("/", async (req, res) => {
         // update student favorites
         await studentModel.findOneAndUpdate(
             // find student
-            { id: ObjectID(studentID) },
+            { id: studentID },
             // set favorites to student favorites
             { $set: { favoriteTutors: tutor } }
         );
@@ -113,6 +129,7 @@ router.delete("/", async (req, res) => {
     // redirect to favorites
     res.redirect("/");
 });
+*/
 
 // Exporting router
 module.exports = router;
